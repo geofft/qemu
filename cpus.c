1f40cd0cbc8ebc22c99cd6f0d2a3e2da8c797a52
Austin Clements <amdragon@mit.edu>
2011-08-10 17:15:01 -0400

    On triple fault, dump state and halt for inspection instead of resetting.
    [geofft@ldpreload.com: Rebase on top of 2.9.0]
    Signed-off-by: Geoffrey Thomas <geofft@ldpreload.com>

--- cpus.c
+++ cpus.c
@@ -1116,6 +1116,17 @@ static void *qemu_kvm_cpu_thread_fn(void *arg)
     do {
         if (cpu_can_run(cpu)) {
             r = kvm_cpu_exec(cpu);
+            if (r == EXCP_TRIPLE) {
+                cpu_dump_state(cpu, stderr, fprintf, 0);
+                fprintf(stderr, "Triple fault.  Halting for inspection via"
+                        " QEMU monitor.\n");
+                if (gdbserver_running())
+                    r = EXCP_DEBUG;
+                else {
+                    vm_stop(RUN_STATE_DEBUG);
+                    break;
+                }
+            }
             if (r == EXCP_DEBUG) {
                 cpu_handle_guest_debug(cpu);
             }
@@ -1356,6 +1367,17 @@ static void *qemu_tcg_rr_cpu_thread_fn(void *arg)
 
                 process_icount_data(cpu);
 
+                if (r == EXCP_TRIPLE) {
+                    cpu_dump_state(cpu, stderr, fprintf, 0);
+                    fprintf(stderr, "Triple fault.  Halting for inspection via"
+                            " QEMU monitor.\n");
+                    if (gdbserver_running())
+                        r = EXCP_DEBUG;
+                    else {
+                        vm_stop(RUN_STATE_DEBUG);
+                        break;
+                    }
+                }
                 if (r == EXCP_DEBUG) {
                     cpu_handle_guest_debug(cpu);
                     break;
@@ -1461,6 +1483,17 @@ static void *qemu_tcg_cpu_thread_fn(void *arg)
         if (cpu_can_run(cpu)) {
             int r;
             r = tcg_cpu_exec(cpu);
+            if (r == EXCP_TRIPLE) {
+                cpu_dump_state(cpu, stderr, fprintf, 0);
+                fprintf(stderr, "Triple fault.  Halting for inspection via"
+                        " QEMU monitor.\n");
+                if (gdbserver_running())
+                    r = EXCP_DEBUG;
+                else {
+                    vm_stop(RUN_STATE_DEBUG);
+                    break;
+                }
+            }
             switch (r) {
             case EXCP_DEBUG:
                 cpu_handle_guest_debug(cpu);
--- gdbstub.c
+++ gdbstub.c
@@ -1939,3 +1939,8 @@ static void register_types(void)
 
 type_init(register_types);
 #endif
+
+int gdbserver_running(void)
+{
+    return gdbserver_state != NULL;
+}

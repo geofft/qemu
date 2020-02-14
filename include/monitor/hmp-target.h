aa129a0c3858029695e6bafbc42f0108bc04f550
Don Porter <porter@cs.unc.edu>
2020-02-13 20:09:18 -0500

    Build fixes

--- include/monitor/hmp-target.h
+++ include/monitor/hmp-target.h
@@ -45,5 +45,6 @@ void hmp_info_tlb(Monitor *mon, const QDict *qdict);
 void hmp_mce(Monitor *mon, const QDict *qdict);
 void hmp_info_local_apic(Monitor *mon, const QDict *qdict);
 void hmp_info_io_apic(Monitor *mon, const QDict *qdict);
+void pg_info(Monitor *mon, const QDict *qdict);
 
 #endif /* MONITOR_COMMON */
